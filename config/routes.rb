6cd4c1cc3844aae7a241d1c0e466866ea866d8a8
Chaz Dean <cdean00@gmail.com>
2022-02-05 13:32:30 -0500

    add controller templates

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
    #root route for react app
   root 'homepage#index'
 
+  namespace :api do
+    resources :dashboard, only: [:index]
+    resources :portfolio_items, except: [:edit, :show] 
+    resources :watchlist_items, only: [:index, :create, :destroy] 
+  end
+
    #catch all routes
    get '/*path' => 'homepage#index'
 end
