0b96ff36e9f5368503e422c2cc835b29150f0b85
Kelsey Erickson <kerickson@gmx.com>
2022-02-05 18:56:28 +0000

    Add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+puts "Creating Users ..."
+
+User.create!({
+  email: "rnarey0@cyberchimps.com",
+  password: "Ripley",
+  first_name: "Simmins",
+  last_name: "Narey",
+  email_notifications: false
+})
+
+User.create!({
+  email: "bbeeken1@hibu.com",
+  password: "Brigit",
+  first_name: "Elsop",
+  last_name: "Beeken",
+  email_notifications: false
+})
+
+User.create!({
+  email: "varmour2@usda.gov",
+  password: "Vladimir",
+  first_name: "Byfford",
+  last_name: "Armour",
+  email_notifications: true
+})
+
+User.create!({
+  email: "asmallwood3@stumbleupon.com",
+  password: "Alard",
+  first_name: "Juckes",
+  last_name: "Smallwood",
+  email_notifications: false
+})
+
+User.create!({
+  email: "oeffemy4@ezinearticles.com",
+  password: "Opalina",
+  first_name: "Gladtbach",
+  last_name: "Effemy",
+  email_notifications: false
+})
+
+puts "Done"
+
