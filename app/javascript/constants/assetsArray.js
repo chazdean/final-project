de2c0e9f22abd7d6333d761b79a910532c859e92
Kelsey Erickson <kerickson@gmx.com>
2022-02-05 19:29:41 +0000

    Add assets array data

---
+++ app/javascript/constants/assetsArray.js
@@ -0,0 +1,182 @@
+const assets = [
+  {
+    symbol: "AAPL",
+    name: "Apple Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "DOCU",
+    name: "Apple Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "AMZN",
+    name: "Amazon.com Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "FB",
+    name: "Meta Platforms Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "GOOGL",
+    name: "Alphabet Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "MSFT",
+    name: "Microsoft Corporation",
+    category: "Common Stock",
+  },
+  {
+    symbol: "NFLX",
+    name: "Netflix Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "TSLA",
+    name: "Tesla Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "V",
+    name: "Visa Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "ZM",
+    name: "Zoom Video Communications",
+    category: "Common Stock",
+  },
+  {
+    symbol: "MCD",
+    name: "McDonald's Corporation",
+    category: "Common Stock",
+  },
+  {
+    symbol: "DIS",
+    name: "The Walt Disney Company",
+    category: "Common Stock",
+  },
+  {
+    symbol: "WMT",
+    name: "Walmart Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "PG",
+    name: "The Proctor & Gamble Company",
+    category: "Common Stock",
+  },
+  {
+    symbol: "SNAP",
+    name: "Snap Inc.",
+    category: "Common Stock",
+  },
+  {
+    symbol: "GME",
+    name: "GameStop Corp",
+    category: "Common Stock",
+  },
+  {
+    symbol: "AMC",
+    name: "AMC Entertainment Holdings",
+    category: "Common Stock",
+  },
+  {
+    symbol: "UBER",
+    name: "UBER Technologies Inc",
+    category: "Common Stock",
+  },
+  {
+    symbol: "XOM",
+    name: "Exxon Mobil Corporation",
+    category: "Common Stock",
+  },
+  {
+    symbol: "PYPL",
+    name: "PayPal Holdings",
+    category: "Common Stock",
+  },
+  {
+    symbol: "SQ",
+    name: "Block Inc.",
+    category: "Common Stock",
+  },
+  { 
+    symbol: "BTC/USD", 
+    name: "Bitcoin", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "ETH/USD", 
+    name: "Ethereum", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "DOGE/USD", 
+    name: "Doge Coin", 
+    category: "Cryptocurrency" 
+  },
+  {
+    symbol: "XRP/USD", 
+    name: "Ripple", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "ADA/USD", 
+    name: "Cardano", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "DOT/USD", 
+    name: "Polkadot", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "SOL/USD", 
+    name: "Solana", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "SHIB/USD", 
+    name: "Shiba Inu", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "ADA/USD", 
+    name: "Cardano", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "AVAX/USD", 
+    name: "Avalanche", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "MATIC/USD", 
+    name: "Polygon", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "DAI/USD", 
+    name: "Dai", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "ATOM/USD", 
+    name: "Cosmos", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "LTC/USD", 
+    name: "Litecoin", 
+    category: "Cryptocurrency" 
+  },
+  { 
+    symbol: "XLM/USD", 
+    name: "Stellar Lumens", 
+    category: "Cryptocurrency" 
+  }
+];
