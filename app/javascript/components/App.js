78f18f63f83160c1f6ea9df40820b06c8b6152f5
Kelsey Erickson <kerickson@gmx.com>
2022-02-05 20:52:30 +0000

    Folder organization

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./Dashboard";
-import Portfolio from "./Portfolio";
+import Dashboard from "./Dashboard/Dashboard";
+import Portfolio from "./Portfolio/Portfolio";
+import NavBar from "./Nav/NavBar";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/portfolio" element={<Portfolio />} />
-            </Routes>
-        </BrowserRouter>
+    <>
+        <NavBar/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                </Routes>
+            </BrowserRouter>
+    </>
     ); 
 }
