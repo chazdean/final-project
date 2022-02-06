70ef4ef7992cbb794c0b49de2a699d445308dc8c
Kelsey Erickson <kerickson@gmx.com>
2022-02-06 01:10:14 +0000

    Add constants file to hold navbar options

---
+++ app/javascript/components/Nav/constants/navbarOptions.js
@@ -0,0 +1,32 @@
+export const navbarOptions = [
+{
+  id: 1,
+  icon: '',
+  label: 'Dashboard', 
+  route: ''
+}, 
+{
+  id: 2,
+  icon: '',
+  label: 'Portfolio', 
+  route: ''
+}, 
+{
+  id: 3,
+  icon: '',
+  label: 'Watchlist', 
+  route: ''
+}, 
+{
+  id: 4,
+  icon: '',
+  label: 'Notifications', 
+  route: ''
+},
+{
+  id: 5,
+  icon: '',
+  label: 'Logout', 
+  route: ''
+}
+]
