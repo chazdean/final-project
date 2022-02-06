073d37502b7675685ec934d23bf872c772e025bf
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-05 23:34:30 -0700

    import temp assets list for testing

--- app/javascript/components/Portfolio/Portfolio.js
+++ app/javascript/components/Portfolio/Portfolio.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+//Components
+import PortfolioForm from './PortfolioForm';
+import { Container } from '@mui/material';
+import { assets } from '../../constants/assetsArray'  //remove once database call is working
+
 
 export default function Portfolio() {
-    return (
-        <div>
-           Portfolio Page
-        </div>
-    );
+  const [pageData, setPageData] = useState({
+    allAssets: assets
+  })
+
+  useEffect(() => {
+    //eventually Promise.all axios calls for all assets list and all portfolio items for the active user
+
+  }, [])
+
+  return (
+    <Container>
+      <PortfolioForm
+        assetsList={pageData.allAssets}
+      />
+    </Container>
+  );
 }
