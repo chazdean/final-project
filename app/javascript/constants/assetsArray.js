b48c4f936c93cdb91c642a6f53e1fb7925c82cfd
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-05 20:02:28 -0700

    changed key to long_name to align with database seeds

--- app/javascript/constants/assetsArray.js
+++ app/javascript/constants/assetsArray.js
@@ -2,301 +2,301 @@ export const assets = [
   {
     id: 1,
     symbol: "AAPL",
-    name: "Apple Inc",
+    long_name: "Apple Inc",
     category: "Common Stock",
   },
   {
     id: 2,
     symbol: "DOCU",
-    name: "Docusign",
+    long_name: "Docusign",
     category: "Common Stock",
   },
   {
     id: 3,
     symbol: "AMZN",
-    name: "Amazon.com Inc",
+    long_name: "Amazon.com Inc",
     category: "Common Stock",
   },
   {
     id: 4,
     symbol: "FB",
-    name: "Meta Platforms Inc",
+    long_name: "Meta Platforms Inc",
     category: "Common Stock",
   },
   {
     id: 5,
     symbol: "GOOGL",
-    name: "Alphabet Inc",
+    long_name: "Alphabet Inc",
     category: "Common Stock",
   },
   {
     id: 6,
     symbol: "MSFT",
-    name: "Microsoft Corporation",
+    long_name: "Microsoft Corporation",
     category: "Common Stock",
   },
   {
     id: 7,
     symbol: "NFLX",
-    name: "Netflix Inc",
+    long_name: "Netflix Inc",
     category: "Common Stock",
   },
   {
     id: 8,
     symbol: "TSLA",
-    name: "Tesla Inc",
+    long_name: "Tesla Inc",
     category: "Common Stock",
   },
   {
     id: 9,
     symbol: "V",
-    name: "Visa Inc",
+    long_name: "Visa Inc",
     category: "Common Stock",
   },
   {
     id: 10,
     symbol: "ZM",
-    name: "Zoom Video Communications",
+    long_name: "Zoom Video Communications",
     category: "Common Stock",
   },
   {
     id: 11,
     symbol: "MCD",
-    name: "McDonald's Corporation",
+    long_name: "McDonald's Corporation",
     category: "Common Stock",
   },
   {
     id: 12,
     symbol: "DIS",
-    name: "The Walt Disney Company",
+    long_name: "The Walt Disney Company",
     category: "Common Stock",
   },
   {
     id: 13,
     symbol: "WMT",
-    name: "Walmart Inc",
+    long_name: "Walmart Inc",
     category: "Common Stock",
   },
   {
     id: 14,
     symbol: "PG",
-    name: "The Proctor & Gamble Company",
+    long_name: "The Proctor & Gamble Company",
     category: "Common Stock",
   },
   {
     id: 15,
     symbol: "SNAP",
-    name: "Snap Inc.",
+    long_name: "Snap Inc.",
     category: "Common Stock",
   },
   {
     id: 16,
     symbol: "GME",
-    name: "GameStop Corp",
+    long_name: "GameStop Corp",
     category: "Common Stock",
   },
   {
     id: 17,
     symbol: "AMC",
-    name: "AMC Entertainment Holdings",
+    long_name: "AMC Entertainment Holdings",
     category: "Common Stock",
   },
   {
     id: 18,
     symbol: "UBER",
-    name: "UBER Technologies Inc",
+    long_name: "UBER Technologies Inc",
     category: "Common Stock",
   },
   {
     id: 19,
     symbol: "XOM",
-    name: "Exxon Mobil Corporation",
+    long_name: "Exxon Mobil Corporation",
     category: "Common Stock",
   },
   {
     id: 20,
     symbol: "PYPL",
-    name: "PayPal Holdings",
+    long_name: "PayPal Holdings",
     category: "Common Stock",
   },
   {
     id: 21,
     symbol: "SQ",
-    name: "Block Inc.",
+    long_name: "Block Inc.",
     category: "Common Stock",
   },
   {
     id: 22,
     symbol: "BTC/USD",
-    name: "Bitcoin",
+    long_name: "Bitcoin",
     category: "Cryptocurrency"
   },
   {
     id: 23,
     symbol: "ETH/USD",
-    name: "Ethereum",
+    long_name: "Ethereum",
     category: "Cryptocurrency"
   },
   {
     id: 24,
     symbol: "DOGE/USD",
-    name: "Doge Coin",
+    long_name: "Doge Coin",
     category: "Cryptocurrency"
   },
   {
     id: 25,
     symbol: "XRP/USD",
-    name: "Ripple",
+    long_name: "Ripple",
     category: "Cryptocurrency"
   },
   {
     id: 26,
     symbol: "ADA/USD",
-    name: "Cardano",
+    long_name: "Cardano",
     category: "Cryptocurrency"
   },
   {
     id: 27,
     symbol: "DOT/USD",
-    name: "Polkadot",
+    long_name: "Polkadot",
     category: "Cryptocurrency"
   },
   {
     id: 28,
     symbol: "SOL/USD",
-    name: "Solana",
+    long_name: "Solana",
     category: "Cryptocurrency"
   },
   {
     id: 29,
     symbol: "SHIB/USD",
-    name: "Shiba Inu",
+    long_name: "Shiba Inu",
     category: "Cryptocurrency"
   },
   {
     id: 30,
     symbol: "AVAX/USD",
-    name: "Avalanche",
+    long_name: "Avalanche",
     category: "Cryptocurrency"
   },
   {
     id: 31,
     symbol: "MATIC/USD",
-    name: "Polygon",
+    long_name: "Polygon",
     category: "Cryptocurrency"
   },
   {
     id: 32,
     symbol: "DAI/USD",
-    name: "Dai",
+    long_name: "Dai",
     category: "Cryptocurrency"
   },
   {
     id: 33,
     symbol: "ATOM/USD",
-    name: "Cosmos",
+    long_name: "Cosmos",
     category: "Cryptocurrency"
   },
   {
     id: 34,
     symbol: "LTC/USD",
-    name: "Litecoin",
+    long_name: "Litecoin",
     category: "Cryptocurrency"
   },
   {
     id: 35,
     symbol: "XLM/USD",
-    name: "Stellar Lumens",
+    long_name: "Stellar Lumens",
     category: "Cryptocurrency"
   },
   {
     id: 36,
     symbol: "TWLO",
-    name: "Twillio Inc",
+    long_name: "Twillio Inc",
     category: "Common Stock"
   },
   {
     id: 37,
     symbol: "PINS",
-    name: "Pinterest Inc",
+    long_name: "Pinterest Inc",
     category: "Common Stock"
   },
   {
     id: 38,
     symbol: "COIN",
-    name: "Coinbase Global Inc",
+    long_name: "Coinbase Global Inc",
     category: "Common Stock"
   },
   {
     id: 39,
     symbol: "MA",
-    name: "Mastercard Inc",
+    long_name: "Mastercard Inc",
     category: "Common Stock"
   },
   {
     id: 40,
     symbol: "DDOG",
-    name: "Datadog Inc",
+    long_name: "Datadog Inc",
     category: "Common Stock"
   },
   {
     id: 41,
     symbol: "ADBE",
-    name: "Adobe Inc",
+    long_name: "Adobe Inc",
     category: "Common Stock"
   },
   {
     id: 42,
     symbol: "COST",
-    name: "Costco Wholesale Corp",
+    long_name: "Costco Wholesale Corp",
     category: "Common Stock"
   },
   {
     id: 43,
     symbol: "INTC",
-    name: "Intel Corporation",
+    long_name: "Intel Corporation",
     category: "Common Stock"
   },
   {
     id: 44,
     symbol: "SBUX",
-    name: "Starbucks Corporation",
+    long_name: "Starbucks Corporation",
     category: "Common Stock"
   },
   {
     id: 45,
     symbol: "ABNB",
-    name: "Airbnb Inc",
+    long_name: "Airbnb Inc",
     category: "Common Stock"
   },
   {
     id: 46,
     symbol: "EBAY",
-    name: "eBay Inc",
+    long_name: "eBay Inc",
     category: "Common Stock"
   },
   {
     id: 47,
     symbol: "CRWD",
-    name: "CrowdStrike Holdings Inc",
+    long_name: "CrowdStrike Holdings Inc",
     category: "Common Stock"
   },
   {
     id: 48,
     symbol: "DPZ",
-    name: "Dominos Pizza Inc",
+    long_name: "Dominos Pizza Inc",
     category: "Common Stock"
   },
   {
     id: 49,
     symbol: "ATVI",
-    name: "Activision Blizzard Inc",
+    long_name: "Activision Blizzard Inc",
     category: "Common Stock"
   },
   {
     id: 50,
     symbol: "BCH/USD",
-    name: "Bitcoin Cash",
+    long_name: "Bitcoin Cash",
     category: "Cryptocurrency"
   }
 ];
