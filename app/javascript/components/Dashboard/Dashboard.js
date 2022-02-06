bdac6b04cda45c4c6bac4eb7fd673035937ede1d
Kelsey Erickson <kerickson@gmx.com>
2022-02-06 20:04:24 +0000

    Add initial dashboard box and grid

--- app/javascript/components/Dashboard/Dashboard.js
+++ app/javascript/components/Dashboard/Dashboard.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import DashboardSummaryItem from './DashboardSummaryItem';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { dashboardStyles } from './styles';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 
 export default function Dashboard() {
     return (
-        <div>
-           HomePage / Dashboard
-        </div>
-    );
+
+    <Box sx={dashboardStyles.box}>
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={4}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid item xs={6} md={8}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+
+          //  <DashboardSummaryItem />
+  
 }
