e3b1ebb1c18ec07c700fb1ac1e8c87e8d2d6d576
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-05 19:50:26 -0700

    create search bar component

---
+++ app/javascript/components/Utilities/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useState } from 'react';
+import { Autocomplete, TextField, Box } from '@mui/material';
+import { assets } from '../../constants/assetsArray'
+
+export default function SearchBar() {
+  const [searchValue, setSearchValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleInput = (event, newInputValue) => {
+    setSearchValue(newInputValue)
+  }
+
+  const handleSelection = (event, newSelection) => {
+    setSelectedValue(newSelection.symbol);
+  }
+
+  return (
+    <Autocomplete
+      id="search-bar"
+      sx={{ width: 600, margin: 10 }}  // may need to revise styles once page component is made
+      options={assets}
+      getOptionLabel={(option) => `${option.name} (${option.symbol})`}
+      noOptionsText={"No Results, try Netflix, Bitcoin, AMZN ..."}
+      disableClearable={true}
+
+      renderInput={(params) => <TextField {...params} label="Search" />}
+
+      value={selectedValue.symbol}
+      onChange={handleSelection}
+      inputValue={searchValue}
+      onInputChange={handleInput}
+    />
+  );
+}
