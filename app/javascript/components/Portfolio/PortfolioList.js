df46a0ecee7e19192fd9148351cab1e07135c62b
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-06 18:13:57 -0700

    initial table body set up working

--- app/javascript/components/Portfolio/PortfolioList.js
+++ app/javascript/components/Portfolio/PortfolioList.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function PortfolioList() {
+//Components
+import PortfolioListItem from './PortfolioListItem'
+import { TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Paper, Box } from '@mui/material';
+
+export default function PortfolioList(props) {
+  const { data } = props
+
   return (
-    <div>
-    </div>
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell align="center">Symbol</TableCell>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Shares</TableCell>
+            <TableCell align="center">Current Price</TableCell>
+            <TableCell align="center">Total Value</TableCell>
+            <TableCell align="center">Percent</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <PortfolioListItem key={row.id} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
