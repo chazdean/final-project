0146668bfca81168bb8eb9ae45e3f7a4764c5c78
Kelsey Erickson <kerickson@gmx.com>
2022-02-07 01:01:20 +0000

    Move data from charts to the Dashboard to pass in as props

--- app/javascript/components/Dashboard/Dashboard.js
+++ app/javascript/components/Dashboard/Dashboard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import DashboardSummaryItem from "./DashboardSummaryItem";
+ //MUI
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import { dashboardStyles } from "./styles";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import DougnutChart from "../charts/DoughnutChart";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+
+ //Charts
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import DougnutChart from "../charts/DoughnutChart";
 import PieChart from "../charts/PieChart";
+
+//Styles
+import { dashboardStyles } from "./styles";
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -20,6 +26,49 @@ export default function Dashboard() {
     investment: "24,6798.93",
     stock: "155,333.65",
     crypto: "91,465.28",
+  };
+
+  const portfolioData = {
+    labels: ["Stocks", "Crypto"], 
+    datasets: [{
+      data: [10, 8], 
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+      ],
+      borderWidth: 1,
+    }]
+  }
+
+  const stockData = {
+    labels: ['NFLX', 'AAPL', 'TSLA', 'SQ', 'GOOGL', 'UBER'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
   };
 
   return (
@@ -36,14 +85,14 @@ export default function Dashboard() {
         <Grid item xs={6} md={4}>
           <Card sx={{ minWidth: 275 }}>
             <CardContent>
-              <DougnutChart />
+              <DougnutChart data={portfolioData} />
             </CardContent>
           </Card>
         </Grid>
         <Grid item xs={6} md={4}>
           <Card sx={{ minWidth: 275 }}>
             <CardContent>
-              <PieChart />
+              <PieChart data={stockData} />
             </CardContent>
           </Card>
         </Grid>
--- app/javascript/components/charts/DoughnutChart.js
+++ app/javascript/components/charts/DoughnutChart.js
@@ -1,28 +1,12 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2"
 
-const data = {
-  labels: ["Stocks", "Crypto"], 
-  datasets: [{
-    data: [10, 8], 
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-    ],
-    borderColor: [
-      'rgba(255, 99, 132, 1)',
-      'rgba(54, 162, 235, 1)',
-    ],
-    borderWidth: 1,
-  }]
-}
-export default function DougnutChart() {
+export default function DougnutChart(props) {
   
   return (
      <div>
-       <h1>Portfolio Breakdown</h1>
        <div>
-       <Doughnut data={data} />
+       <Doughnut data={props.data} />
        </div>
      </div>
   )
