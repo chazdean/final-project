9fa6491edb217cd0b2d4c2f67cd133b52a516e85
Chaz Dean <cdean00@gmail.com>
2022-02-06 20:07:25 -0500

    fixed PortfolioItemsController index

--- app/controllers/api/portfolio_items_controller.rb
+++ app/controllers/api/portfolio_items_controller.rb
@@ -1,20 +1,34 @@
 class Api::PortfolioItemsController < ApplicationController
     def show
-        @portfolio_items = PortfolioItem.where(user_id: params[:id])
-        #get asset data for each item
-        @portfolio_params = get_porfolio_params(@portfolio_items).join(',')
-        new_portfolio_items = @portfolio_items.map { |item| item.attributes }
-        @current_prices = get_current_prices(@portfolio_params)
+        portfolio_items = PortfolioItem.where(user_id: params[:id])
+       
+        #Create array of all portfolio items
+        new_portfolio_items = portfolio_items.map { |item| item.attributes }
 
-        new_portfolio_items.each { |item| 
-            add_attr_current_price(item)
-            add_attr_total_value(item)
-            item.delete("created_at")
-            item.delete("updated_at")
+        #Create array of asset items
+        asset_items = portfolio_items.map { |item| 
+            asset = Asset.where(id: item.asset_id)
+            asset[0].attributes
         }
 
+        #Creates string of stock symbols needed for api call
+        asset_api_params = get_asset_api_params(asset_items).join(',')
+
+        #Returns json of all current prices of stock symbols
+        @current_prices = get_current_prices(asset_api_params)
+
+        #Updates new_portfolio array by adding info from assets array and other data
+        new_portfolio_items.each_with_index { |item, index| 
+            item["symbol"] = asset_items[index]["symbol"]
+            item["long_name"] = asset_items[index]["long_name"]
+            item["category"] = asset_items[index]["category"]
+            add_attr_current_price(item)
+            add_attr_total_value(item)
+        }
+        
+        #Returns new_portfolio array with [percent_of_portfolio] for each item
         add_attr_percent_of_portfolio(new_portfolio_items)
-            
+
         render json: new_portfolio_items
     end
 
@@ -30,15 +44,14 @@ class Api::PortfolioItemsController < ApplicationController
         #portfolio_item_id
     end
 
-
     private 
 
     def portfolio_item_params
         params.require(:portfolio_item).permit(:id)
     end
 
-    def get_porfolio_params(portfolio_items_array)
-        portfolio_items_array.map { |item| item.symbol }
+    def get_asset_api_params(asset_items_array)
+        asset_items_array.map { |item| item["symbol"] }
     end
 
     def get_current_prices(symbol_params)
@@ -65,18 +78,18 @@ class Api::PortfolioItemsController < ApplicationController
     end
 
     def add_attr_total_value(portfolio_item)
-        portfolio_item["total-value"] = get_total_value(portfolio_item)
+        portfolio_item["total_value"] = get_total_value(portfolio_item)
     end
 
     def add_attr_percent_of_portfolio(portfolio_items)
         portfolio_value = 0
 
         portfolio_items.each { |item| 
-            portfolio_value += item["total-value"]
+            portfolio_value += item["total_value"]
         }
     
         portfolio_items.each { |item| 
-            percent_of_portfolio = item["total-value"].to_f / portfolio_value
+            percent_of_portfolio = item["total_value"].to_f / portfolio_value
             item["percent_of_portfolio"] = (percent_of_portfolio * 100).round(2)
         }
     end
