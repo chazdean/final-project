38e6c45f89d8209e3edd0d2dacb50542dbfd4036
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-06 20:43:34 -0700

    add styles for update form

--- app/javascript/components/Portfolio/PortfolioListitem.js
+++ app/javascript/components/Portfolio/PortfolioListitem.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react'
 
 //Components
-import { TableRow, TableCell, TableHead, IconButton, Collapse, Box, Typography } from '@mui/material';
+import { TableRow, TableCell, IconButton, Collapse, Box, Typography, TextField, Grid, Button, Container } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 
 export default function PortfolioListItem(props) {
   const { asset } = props;
 
   const [open, setOpen] = useState(false);
+  const [newShares, setNewShares] = useState('');
+
+  const handleUpdate = () => {
+    console.log("update clicked")
+  }
+
+  const handleDelete = () => {
+    console.log("delete clicked")
+  }
 
   return (
     <>
@@ -27,18 +37,41 @@ export default function PortfolioListItem(props) {
         <TableCell align="center">{asset.symbol}</TableCell>
         <TableCell align="center">{asset.long_name}</TableCell>
         <TableCell align="center">{asset.shares}</TableCell>
-        <TableCell align="center" scope='integer'>{asset.price}</TableCell>
+        <TableCell align="center">{asset.price}</TableCell>
         <TableCell align="center">{asset.total_value}</TableCell>
         <TableCell align="center">{asset.percent}</TableCell>
       </TableRow>
 
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
           <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                Insert update form and button here
-              </Typography>
+
+            <Box sx={{ padding: 2, display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
+
+              <Box sx={{ paddingRight: 25, display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
+                <TextField
+                  label="New Shares"
+                  variant="outlined"
+                  color="secondary"
+                  onChange={(event) => setNewShares(event.target.value)}
+                  value={newShares}
+                />
+                <Button
+                  sx={{ marginLeft: 5 }}
+                  type='submit'
+                  color="secondary"
+                  variant="contained"
+                  onClick={handleUpdate}
+                >Update</Button>
+              </Box>
+
+              <Button
+                color="error"
+                variant="contained"
+                startIcon={<HighlightOffIcon />}
+                onClick={handleDelete}
+              >Delete</Button>
+
             </Box>
           </Collapse>
         </TableCell>
