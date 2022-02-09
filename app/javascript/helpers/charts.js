24cc49f7c55da948df0431d8fb9ef7779e5ed031
Kelsey Erickson <kerickson@gmx.com>
2022-02-09 23:24:11 +0000

    Add function to return chart data for portfolio percentages

--- app/javascript/helpers/charts.js
+++ app/javascript/helpers/charts.js
@@ -1,4 +1,4 @@
-export const chartData = (assetList) => {
+export const assetChartData = (assetList) => {
   let labels = [];
   let data = [];
 
@@ -35,3 +35,34 @@ export const chartData = (assetList) => {
 
   return chartData;
 };
+
+export const totalPercentData = (stockPercent, cryptoPercent) => {
+  let chartData = {
+    labels: ["Stocks", "Crypto"],
+    datasets: [
+      {
+        data: [stockPercent, cryptoPercent],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return chartData;
+};
+
