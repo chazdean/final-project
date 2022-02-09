e08f243414d09098aa853d99421bbaf35fd18b8a
Kelsey Erickson <kerickson@gmx.com>
2022-02-09 23:06:36 +0000

    Refactor chartData function

--- app/javascript/helpers/charts.js
+++ app/javascript/helpers/charts.js
@@ -1,39 +1,37 @@
 export const chartData = (assetList) => {
-const labels = [];
-const data = [];
+  let labels = [];
+  let data = [];
 
-const labelArray = assetList.map((asset) => {
-  labels.push(asset.symbol)
-})
+  assetList.map((asset) => {
+    labels.push(asset.symbol);
+    data.push(asset.percent_of_portfolio);
+  });
 
-const chartDataArray = assetList.map((asset) => {
-  data.push(asset.percent)
-})
+  let chartData = {
+    labels: labels,
+    datasets: [
+      {
+        data: data,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-return chartData = {
-  labels: labels,
-  datasets: [
-    {
-      data: data,
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
+  return chartData;
 };
-
-}
