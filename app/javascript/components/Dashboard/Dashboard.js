0614d5e3c78f92f890e196d29c36acfc38a1d256
Kelsey Erickson <kerickson@gmx.com>
2022-02-09 23:53:57 +0000

    Add portfolio percentage data and add watchlist

--- app/javascript/components/Dashboard/Dashboard.js
+++ app/javascript/components/Dashboard/Dashboard.js
@@ -1,21 +1,18 @@
 import React from "react";
+
+//Components
 import SummaryCard from "./SummaryCard";
 import BreakDown from "./BreakDown";
+import WatchList from "./WatchList";
 
 //Helpers
 import { sortStocks, sortCryptos } from "../../helpers/sort";
 import { totalValue, totalPercent } from "../../helpers/totals";
-import { chartData } from "../../helpers/charts";
+import { assetChartData, totalPercentChartData } from "../../helpers/charts";
 
 //MUI
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-// import ChartDataLabels from "chartjs-plugin-datalabels";
-
-//Router
-import { Link } from "react-router-dom";
 
 //Charts
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
@@ -25,21 +22,26 @@ import { dashboardStyles } from "./styles";
 
 //Registering Charts
 ChartJS.register(ArcElement, Tooltip, Legend);
-//ChartJS.register(ChartDataLabels);
+
 
 export default function Dashboard(props) {
+
   const { portfolioItems } = props;
 
   const stockList = sortStocks(portfolioItems);
   const cryptoList = sortCryptos(portfolioItems);
 
+
   const totalPortfolioValue = totalValue(portfolioItems);
   const totalStockValue = totalValue(stockList);
   const totalCryptoValue = totalValue(cryptoList);
 
-  const stockChartData = chartData(stockList)
-  const cryptoChartData = chartData(cryptoList)
+  const totalStockPercent = totalPercent(stockList);
+  const totalCryptoPercent = totalPercent(stockList);
 
+  const portfolioChartData = totalPercentChartData(totalStockPercent, totalCryptoPercent);
+  const stockChartData = assetChartData(stockList);
+  const cryptoChartData = assetChartData(cryptoList);
 
   return (
     <Box sx={dashboardStyles.box}>
@@ -62,23 +64,29 @@ export default function Dashboard(props) {
         />
       </Grid>
       <Grid container spacing={2} sx={dashboardStyles.gridCharts}>
-        {/* <BreakDown data={portfolioItems} link={"/portfolio"} pieChart={false} /> */}
-        <BreakDown data={stockChartData} link={"/portfolio"} pieChart={true} />
-        <BreakDown data={cryptoChartData} link={"/portfolio"} pieChart={true} />
+        <BreakDown 
+          data={portfolioChartData} 
+          link={"/portfolio"} 
+          pieChart={false} 
+        />
+        <BreakDown 
+          data={stockChartData} 
+          link={"/portfolio"} 
+          pieChart={true} 
+        />
+        <BreakDown 
+          data={cryptoChartData} 
+          link={"/portfolio"} 
+          pieChart={true} 
+        />
       </Grid>
-      {/* <Grid container spacing={2}>
-        <Grid item xs={6} md={8} sx={dashboardStyles.watchList}>
-          <Link to={"/watchlist"} style={{ textDecoration: "none" }}>
-            <Card sx={{ minWidth: 275 }}>
-              <CardContent>
-                <h1>
-                  Watch List: <span>10 Stocks, 7 Crypto</span>
-                </h1>
-              </CardContent>
-            </Card>
-          </Link>
-        </Grid>
-      </Grid> */}
+      <Grid container spacing={2}>
+        <WatchList 
+          stockCount={stockList.length}
+          cryptoCount={cryptoList.length}
+          link={"/watchlist"}
+        />
+      </Grid>
     </Box>
   );
 }
