406cbf5fd4f8fdea841c39dec1c04c2716eb2bb4
Kelsey Erickson <kerickson@gmx.com>
2022-02-09 22:32:37 +0000

    Add helper file to insert data into the charts

---
+++ app/javascript/helpers/charts.js
@@ -0,0 +1,39 @@
+export const chartData = (assetList) => {
+const labels = [];
+const data = [];
+
+const labelArray = assetList.map((asset) => {
+  labels.push(asset.symbol)
+})
+
+const chartDataArray = assetList.map((asset) => {
+  data.push(asset.percent)
+})
+
+return chartData = {
+  labels: labels,
+  datasets: [
+    {
+      data: data,
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+}
