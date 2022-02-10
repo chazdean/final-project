3c64330190b5e4232af99a3688b4587162636fb4
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-10 16:45:04 -0700

    add call for deleteItem to handleDelete

--- app/javascript/components/Portfolio/PortfolioList.js
+++ app/javascript/components/Portfolio/PortfolioList.js
@@ -5,7 +5,16 @@ import PortfolioListItem from './PortfolioListItem'
 import { TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Paper } from '@mui/material';
 
 export default function PortfolioList(props) {
-  const { data } = props
+  const { data, deleteItem } = props
+
+  const handleUpdate = () => {
+    console.log(`update clicked on item ${portfolioItem.id}`)
+    // onUpdate(newShares, portfolioItem.id)
+  }
+
+  const handleDelete = (id) => {
+    deleteItem("portfolio", id)
+  }
 
   return (
     <TableContainer component={Paper}>
@@ -16,14 +25,19 @@ export default function PortfolioList(props) {
             <TableCell align="center">Symbol</TableCell>
             <TableCell align="center">Name</TableCell>
             <TableCell align="center">Shares</TableCell>
-            <TableCell align="center">Current Price</TableCell>
+            <TableCell align="center">Current Price (USD)</TableCell>
             <TableCell align="center">Total Value</TableCell>
             <TableCell align="center">Percent</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((portfolioItem) => (
-            <PortfolioListItem key={portfolioItem.id} portfolioItem={portfolioItem} />
+            <PortfolioListItem
+              key={portfolioItem.id}
+              portfolioItem={portfolioItem}
+              handleDelete={handleDelete}
+              handleUpdate={handleUpdate}
+            />
           ))}
         </TableBody>
       </Table>
