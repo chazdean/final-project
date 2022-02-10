c11dadc92fecbf676b345b07bbc5af94833c3c1a
Kelsey Erickson <kerickson@gmx.com>
2022-02-10 23:47:42 +0000

    Add money formatter helper function

--- app/javascript/helpers/formatting.js
+++ app/javascript/helpers/formatting.js
@@ -4,3 +4,4 @@ export const formatter = new Intl.NumberFormat('en-US', {
 });
 
 // use as follows: formatter.format(2500)
+// I used it like this: <h2>{formatter.format(props.total)}</h2> and it rendered all my cards formatted
