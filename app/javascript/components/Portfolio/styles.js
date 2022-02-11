aedbd29012c7a2217cccdfdd4d16259a68a9c4ed
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-10 22:54:08 -0700

    create styles for portfolio components

---
+++ app/javascript/components/Portfolio/styles.js
@@ -0,0 +1,52 @@
+export const portfolioStyles = {
+  title: {
+    color: '#7b1fa2',
+    mb: 6
+  },
+
+  box: {
+    flexGrow: 1,
+    ml: 30,
+    mr: 3,
+    mt: 5,
+    mb: 10
+  },
+
+  icon: {
+    pr: 2,
+    height: 40,
+    width: 'auto'
+  },
+
+  headPaper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '70%'
+
+  },
+
+  summaryCard: {
+    p: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+
+  },
+
+  formCard: {
+    p: 3,
+    display: 'box',
+    width: '600px'
+  },
+
+  formButtonBox: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    pr: 5
+  },
+
+  tableCell: {
+    width: 130
+  }
+
+};
