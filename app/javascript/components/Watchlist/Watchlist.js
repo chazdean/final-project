f0133547a970c288b1efe8f03cb51a69df660bff
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-11 16:56:25 -0700

    init Watchlist component, implement summary card

---
+++ app/javascript/components/Watchlist/Watchlist.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+//Components
+import SummaryCard from './SummaryCard';
+import { Box, Typography, Paper } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+//Constants & Helpers
+import { sortStocks, sortCryptos } from '../../helpers/sort'
+import { assets } from '../../constants/assetsArray'  //remove once database call is working
+
+//Styles
+import { watchlistStyles } from "./styles"
+
+
+export default function Watchlist(props) {
+
+  const { watchlistItems, addWatchlistItem, deleteItem } = props;
+
+  const stockList = sortStocks(watchlistItems);
+  const cryptoList = sortCryptos(watchlistItems);
+
+  return (
+    <Box sx={watchlistStyles.box}>
+
+      <Typography gutterBottom variant='h3' sx={watchlistStyles.title}>
+        <VisibilityIcon sx={watchlistStyles.icon} />
+        Watchlist
+      </Typography>
+
+      <Box>
+        <Paper elevation={3} sx={watchlistStyles.headPaper}>
+          <SummaryCard
+            stockList={stockList}
+            cryptoList={cryptoList}
+          />
+          {/* <WatchlistForm
+            assetsList={assets}
+            addWatchlistItem={addWatchlistItem}
+          /> */}
+        </Paper>
+      </Box>
+
+      {/* <Box mt={8}>
+        <Typography color="secondary" variant="subtitle1" mb={2}>Stocks</Typography>
+        <WatchlistList
+          data={stockList}
+          deleteItem={deleteItem}
+        />
+      </Box>
+
+      <Box mt={8}>
+        <Typography color="secondary" variant="subtitle1" mb={2}>Cryptocurrency</Typography>
+        <WatchlistList
+          data={cryptoList}
+          deleteItem={deleteItem}
+        />
+      </Box> */}
+
+    </Box>
+  );
+}
