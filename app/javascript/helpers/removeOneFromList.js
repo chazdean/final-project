8577fb6877f5e36649d1c52cacb60876cf776188
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-11 12:01:02 -0700

    implement updatePercents for portfolio lists

--- app/javascript/helpers/removeOneFromList.js
+++ app/javascript/helpers/removeOneFromList.js
@@ -1,4 +1,12 @@
-export const removeOneFromList = (currentArray, id) => {
+import { updatePercents } from "./updatePercents";
+
+/**
+ * removeOneFromList
+ * @param {arr of obj} currentArray is the current state of portfolio items array 
+ * @param {*} id is the portfolio_item_id that is being removed
+ * @returns an updated state array with the item removed and updated percents for remaining items if it is the portfolio list
+ */
+export const removeOneFromList = (listName, currentArray, id) => {
   const index = currentArray.findIndex(item => item.id === id);
 
   const newArray = currentArray.filter((obj, ind) => {
@@ -7,5 +15,7 @@ export const removeOneFromList = (currentArray, id) => {
     }
   })
 
-  return newArray
+  if (listName === "portfolio") return updatePercents(newArray);
+
+  return newArray;
 };
