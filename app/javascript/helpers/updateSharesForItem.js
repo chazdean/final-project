e6436e46c39b37d147780a6ba8f1e2e54c2923f4
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-11 11:39:41 -0700

    implement updatePercents helper

--- app/javascript/helpers/updateSharesForItem.js
+++ app/javascript/helpers/updateSharesForItem.js
@@ -1,12 +1,22 @@
+import { updatePercents } from "./updatePercents";
+
+
+/**
+ * updateSharesForItem
+ * @param {arr of obj} currentArray is the current state of portfolio items array 
+ * @param {int} newShares is the updated number of shares owned by the user
+ * @param {int} id is the portfolio_item_id that is being updated
+ * @returns an updated state array with adjusted total value and percents 
+ */
 export const updateSharesForItem = (currentArray, newShares, id) => {
   const index = currentArray.findIndex(item => item.id === id);
 
   const newArray = currentArray.map((obj, ind) => {
     if (ind === index) {
-      return { ...obj, shares: newShares }
+      return { ...obj, shares: newShares, total_value: Math.round(newShares * obj.price * 100) / 100 }
     }
     return { ...obj }
   })
 
-  return newArray
+  return updatePercents(newArray)
 };
