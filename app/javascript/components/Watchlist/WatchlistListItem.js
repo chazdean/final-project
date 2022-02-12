4ddace2f5e1a991bc64ca1035201e9a1447370d2
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-11 17:38:25 -0700

    create watchlist tables and items

---
+++ app/javascript/components/Watchlist/WatchlistListItem.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+//Components
+import { TableRow, TableCell, IconButton, Collapse, Box, Typography, TextField, Grid, Button, Container } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
+
+export default function WatchlistListItem(props) {
+  const { watchlistItem, handleDelete } = props;
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <EditIcon />}
+          </IconButton>
+        </TableCell>
+
+        <TableCell align="center">{watchlistItem.symbol}</TableCell>
+        <TableCell align="center">{watchlistItem.long_name}</TableCell>
+        <TableCell align="center">{Number(watchlistItem.price).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</TableCell>
+        <TableCell align="center" sx={(Number(watchlistItem.percent_change) < 0) ? { color: 'red' } : { color: 'green' }}>{Math.round(watchlistItem.percent_change * 100) / 100}%</TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+
+            <Box sx={{ padding: 2, display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+              <Button
+                color="error"
+                variant="contained"
+                startIcon={<HighlightOffIcon />}
+                onClick={() => handleDelete(watchlistItem.id)}
+              >Delete</Button>
+            </Box>
+
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
