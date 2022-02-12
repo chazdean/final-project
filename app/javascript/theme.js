ad12f8df1094ec19bdf1f9149f9f14034a513f1f
Kelsey Erickson <kerickson@gmx.com>
2022-02-12 21:36:22 +0000

    Comments

--- app/javascript/theme.js
+++ app/javascript/theme.js
@@ -4,21 +4,21 @@ const primaryColor = "#6E3CBC";
 const secondaryColor = "#289ebf";
 
 const theme = createTheme({
-  // typography: { // seems to not be applied to the dashboard cards, but is working on everything else
-  //   fontFamily: [
-  //     '-apple-system',
-  //     'BlinkMacSystemFont',
-  //     '"Segoe UI"',
-  //     'Roboto',
-  //     '"Helvetica Neue"',
-  //     'Arial',
-  //     'sans-serif',
-  //     '"Apple Color Emoji"',
-  //     '"Segoe UI Emoji"',
-  //     '"Segoe UI Symbol"',
-  //   ].join(','),
-  //   fontSize: 15,
-  // }, 
+  typography: { // seems to not be applied to the dashboard cards, but is working on everything else
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    fontSize: 15,
+  }, 
   palette: {
     type: "light",
     primary: {
