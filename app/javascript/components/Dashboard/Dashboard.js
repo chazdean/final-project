6ab0961fa33b44f924cd5e5f708a74e169b1c5d3
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-13 13:20:33 -0700

    reorder components and add userCard

--- app/javascript/components/Dashboard/Dashboard.js
+++ app/javascript/components/Dashboard/Dashboard.js
@@ -3,16 +3,20 @@ import React from "react";
 //Components
 import SummaryCard from "./SummaryCard";
 import BreakDown from "./BreakDown";
-import WatchList from "./WatchList";
+import CountCard from "./CountCard";
+import UserCard from "./UserCard";
 
 //Helpers
 import { sortStocks, sortCryptos } from "../../helpers/sort";
-import { totalValue, totalPercent, percentOfAsset } from "../../helpers/totals";
-import { assetChartData, totalPercentChartData, chartOptions } from "../charts/helpers/chartsData";
+import { totalValue, percentOfAsset } from "../../helpers/totals";
+import { stockChartData, cryptoChartData, totalPercentChartData, chartOptions } from "../charts/helpers/chartsData";
 
 //MUI
 import { Box, Grid, Typography } from "@mui/material";
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import PersonIcon from '@mui/icons-material/Person';
 
 //Charts
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
@@ -26,11 +30,13 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Dashboard(props) {
 
-  const { portfolioItems } = props;
+  const { portfolioItems, watchlistItems } = props;
 
   //List of Assets
   const stockList = sortStocks(portfolioItems);
   const cryptoList = sortCryptos(portfolioItems);
+  const watchStockList = sortStocks(watchlistItems);
+  const watchCryptoList = sortCryptos(watchlistItems);
 
   // Total Value for Top Totals
   const totalPortfolioValue = totalValue(portfolioItems);
@@ -47,13 +53,13 @@ export default function Dashboard(props) {
 
   // Chart Data
   const portfolioChartData = totalPercentChartData(portfolioStockPercentage, portfolioCryptoPercentage);
-  const stockChartData = assetChartData(stockList, totalStockPercent);
-  const cryptoChartData = assetChartData(cryptoList, totalCryptoPercent);
+  const stockChart = stockChartData(stockList, totalStockPercent);
+  const cryptoChart = cryptoChartData(cryptoList, totalCryptoPercent);
 
   //Chart Options
   const portfolioChartOptions = chartOptions(portfolioChartData)
-  const stockChartOptions = chartOptions(stockChartData)
-  const cryptoChartOptions = chartOptions(cryptoChartData)
+  const stockChartOptions = chartOptions(stockChart)
+  const cryptoChartOptions = chartOptions(cryptoChart)
 
 
   return (
@@ -63,7 +69,28 @@ export default function Dashboard(props) {
         DASHBOARD
       </Typography>
 
-      <Grid container spacing={2}>
+      <Grid container spacing={3} sx={dashboardStyles.gridBox}>
+        <UserCard
+          userName={"Jackson Five"}
+          icon={<PersonIcon />}
+        />
+        <CountCard
+          title={"Portfolio Assets"}
+          stockCount={stockList.length}
+          cryptoCount={cryptoList.length}
+          link={"/portfolio"}
+          icon={<ShowChartIcon />}
+        />
+        <CountCard
+          title={"Watchlist Assets"}
+          stockCount={watchStockList.length}
+          cryptoCount={watchCryptoList.length}
+          link={"/watchlist"}
+          icon={<VisibilityIcon />}
+        />
+      </Grid>
+
+      <Grid container spacing={3}>
         <SummaryCard
           title={"Total Investments"}
           total={totalPortfolioValue}
@@ -77,13 +104,13 @@ export default function Dashboard(props) {
           name={"stocks"}
         />
         <SummaryCard
-          title={"Total Crypto"}
+          title={"Total Cryptocurrency"}
           total={totalCryptoValue}
           link={"/portfolio"}
           name={"crypto"}
         />
       </Grid>
-      <Grid container spacing={2} sx={dashboardStyles.gridCharts}>
+      <Grid container spacing={3} sx={dashboardStyles.gridBox}>
         <BreakDown
           data={portfolioChartData}
           options={portfolioChartOptions}
@@ -91,23 +118,16 @@ export default function Dashboard(props) {
           pieChart={false}
         />
         <BreakDown
-          data={stockChartData}
+          data={stockChart}
           options={stockChartOptions}
           link={"/portfolio"}
           pieChart={true}
         />
         <BreakDown
-          data={cryptoChartData}
+          data={cryptoChart}
           options={cryptoChartOptions}
           link={"/portfolio"}
           pieChart={true}
-        />
-      </Grid>
-      <Grid container spacing={2}>
-        <WatchList
-          stockCount={stockList.length}
-          cryptoCount={cryptoList.length}
-          link={"/watchlist"}
         />
       </Grid>
     </Box>
