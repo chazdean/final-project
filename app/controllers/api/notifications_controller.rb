c32aad686a71719ed502f57c19e84983adef4c8a
KelseyErickson <kerickson@gmx.com>
2022-02-14 15:53:11 -0700

    Removed the mailer from the update task

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -10,10 +10,8 @@ class Api::NotificationsController < ApplicationController
 
   def update
     @user = User.where(id: 1)
-    puts @user
-   if User.update(params[:id], :email_notifications => user_params[:email_notifications]) && user_params[:email_notifications] == true
-    ProfileMailer.profile_summary_send.deliver_later
-   end
+   User.update(params[:id], :email_notifications => user_params[:email_notifications])
+    # ProfileMailer.profile_summary_send.deliver_later
     
   end
 
