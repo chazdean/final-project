53a2dc2c3774428aed16d1337c94923bbd858f0e
KelseyErickson <kerickson@gmx.com>
2022-02-13 20:47:02 -0700

    Add update for email notifications

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -1,4 +1,22 @@
-class NotificationsController < ApplicationController
+class Api::NotificationsController < ApplicationController
   def index
+    @users = User.where(id: 1) #hard coded in for now
+    render json: @users
   end
+
+  def show
+
+  end
+
+  def update
+    User.update(params[:id], :email_notifications => user_params[:email_notifications])
+  end
+
+private 
+
+def user_params
+    params.require(:params).permit(:email_notifications)
 end
+
+end
+
