fbdd365e7792d6a0b92779bac63fb8d8adf5f1c8
KelseyErickson <kerickson@gmx.com>
2022-02-13 20:49:24 -0700

    Add logic to update state for email notifications

--- app/javascript/components/notifications/Notifications.js
+++ app/javascript/components/notifications/Notifications.js
@@ -1,14 +1,44 @@
-import React from "react";
-import { FormGroup, FormControlLabel, Switch } from "@mui/material";
-
+import React, { useEffect, useState } from "react";
+import { FormGroup, FormControlLabel, Switch, Button } from "@mui/material";
+import axios from "axios";
 
 export default function Notifications() {
-  
-  return (
+  const [userData, setUserData] = useState({});
+  const [toggle, setToggle] = useState(false)
 
-    <FormGroup sx={{ml: 30}} >
-    <FormControlLabel control={<Switch defaultChecked />} label="Notifications" />
-    <FormControlLabel disabled control={<Switch />} label="Disabled" />
-  </FormGroup>
+  useEffect(() => {
+    axios.get(`http://localhost:3000/api/notifications/`).then((data) => {
+      setUserData(data.data[0]);
+      setToggle(data.data[0].email_notifications)
+    });
+  }, []);
+
+  
+  const handleSubmit = () => {
+    const params = {
+      email_notifications: toggle
+    }
+    return axios
+      .patch(`http://localhost:3000/api/notifications/1`, { params })
+      .then(() => {
+        setUserData((prev) => ({ ...prev, email_notifications: params.email_notifications }))
+      });
+  };
+
+
+  return (
+      <FormGroup sx={{ ml: 30 }}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={toggle}
+              onChange={(event) => setToggle(event.target.checked)}
+            />
+          }
+          label="Notifications"
+        />
+        <FormControlLabel disabled control={<Switch />} label="Disabled" />
+        <Button onClick={handleSubmit}>Save</Button>
+      </FormGroup>
   );
 }
