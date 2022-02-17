16d6c804251d1e3cd7d8ce1c596a06e8dd70752d
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-16 22:45:24 -0700

    form compoent rendering with image

--- app/javascript/components/Login/LoginForm.js
+++ app/javascript/components/Login/LoginForm.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react'
 
 //Components
-import { Box, Typography, TextField, Grid, Button } from '@mui/material';
+import { Box, Typography, TextField, Grid, Button, CssBaseline, Avatar, Paper, Link } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 
 //Helpers
 
 //Styles
 import { loginStyles } from "./styles";
+import loginPhoto from "../../../../public/login-photo"
 
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="rgba(0,0,0,0.71)" align="center">
+      <Link color="inherit" href="https://github.com/chazdean/final-project">
+        Github Repo
+      </Link>{' '}
+      {' Copyright © '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 export default function LoginForm(props) {
   const { session } = props;
@@ -16,12 +30,88 @@ export default function LoginForm(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleSubmit = () => {
+    console.log('Login selected')
+  }
+
   return (
-    <Box sx={loginStyles.box}>
-      <Typography gutterBottom variant='h3' sx={loginStyles.title}>
-        <PersonIcon sx={loginStyles.icon} />
-        LOGIN
-      </Typography>
-    </Box>
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: `url(${loginPhoto})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: (t) =>
+            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: '#289ebf' }}>
+            <PersonIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Copyright sx={{ mt: 5 }} />
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
