119fbceedef16b0762096af1b880134ff9207305
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-16 22:46:04 -0700

    add styles for login form

---
+++ app/javascript/components/Login/styles.js
@@ -0,0 +1,41 @@
+export const loginStyles = {
+  title: {
+    mb: 5
+  },
+
+  box: {
+    maxHeight: "90%",
+    maxWidth: "80%",
+    flexGrow: 1,
+    ml: '260px',
+    pr: 4,
+    pt: 4,
+  },
+
+  icon: {
+    color: 'rgba(0,0,0,0.71)',
+    pr: 3,
+    height: 45,
+    width: 'auto'
+  },
+
+  headPaper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '80%'
+
+  },
+
+  formCard: {
+    p: 3,
+    display: 'box',
+    width: '600px'
+  },
+
+  formButtonBox: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    pr: 5
+  }
+
+};
