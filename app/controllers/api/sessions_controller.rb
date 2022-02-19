78c06eb26e0e3d9b202043bd626bb00ebf8b1fcd
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-18 22:54:03 -0700

    update create route to find user by email

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,10 @@
 class Api::SessionsController < ApplicationController
 
   def create
-      # If the user exists AND the password entered is correct.
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user.id
+    @user = User.find_by(email: params[:email])  
+    # If the user exists AND the password entered is correct.
+    if @user
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       # If user's login doesn't work, send them back to the login form.
@@ -16,6 +17,5 @@ class Api::SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to '/login'
   end
-
 
 end
