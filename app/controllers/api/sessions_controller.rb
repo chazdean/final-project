3b417abc4066bb734de1734ea8088d1ba823ce7c
Justin Wheeler <justinwwheeler@gmail.com>
2022-02-19 13:30:28 -0700

    add session show route to check for user_id

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,21 +1,23 @@
 class Api::SessionsController < ApplicationController
 
+  def show
+    render json: session[:user_id]
+  end
+  
   def create
     @user = User.find_by(email: params[:email])  
+    
     # If the user exists AND the password entered is correct.
     if @user
       session[:user_id] = @user.id
-      redirect_to '/'
     else
       # If user's login doesn't work, send them back to the login form.
       p "User Login Failed"
-      redirect_to '/login'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
   end
 
 end
