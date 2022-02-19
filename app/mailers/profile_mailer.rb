ed20c681c190bb2fe26d0c0ceb2ebb0cf72a0eb7
KelseyErickson <kerickson@gmx.com>
2022-02-18 19:10:41 -0700

    Add lgo image as attachment to email

--- app/mailers/profile_mailer.rb
+++ app/mailers/profile_mailer.rb
@@ -3,6 +3,7 @@ class ProfileMailer < ApplicationMailer
     @user = params[:user]
     @portfolio = params[:portfolio]
     @watchlist = params[:watchlist]
+    attachments.inline["rocketLogoNoBG.png"] = File.read("#{Rails.root}/public/rocketLogoNoBG.png")
     mail(
       to: ENV["USER_EMAIL"],  ## this will be @user.email 
       subject: "Daily Profile Summary - To The Moon!"
