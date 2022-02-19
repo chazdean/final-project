f50424070249ae1bbe2576b8ecbc22026f27554e
KelseyErickson <kerickson@gmx.com>
2022-02-18 19:07:44 -0700

    Add styles for notifcations

---
+++ app/javascript/components/notifications/styles.js
@@ -0,0 +1,27 @@
+export const notificationStyles = {
+  title: {
+    mb: 5
+  },
+
+  box: {
+    maxHeight: "90%",
+    maxWidth: "80%",
+    flexGrow: 1,
+    ml: '260px',
+    pr: 4,
+    pt: 4,
+  },
+
+  icon: {
+    color: 'rgba(0,0,0,0.71)',
+    pr: 3,
+    height: 45,
+    width: 'auto'
+  },
+
+  button: {
+    maxWidth: 5,
+    mt: 5,
+  },
+
+};
